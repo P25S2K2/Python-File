b76346e5c9381973ea70bc9785bc1d133fe46398
P25S2K2 <109511932+P25S2K2@users.noreply.github.com>
2022-07-20 14:29:59 +0530

    first commit

---
+++ Control Flow Statements.py
@@ -0,0 +1,23 @@
+# 1. Conditional Statement (if , if else)
+
+a=6
+b=3
+if(a>4):  #if Statement(if)
+   if(b<4):
+      print('Hello')
+   print('Puneet')
+   
+
+a=2
+b=3
+if(a>b):  #if else Statement(if else) 
+   print('Puneet')
+else:
+    print('Kalsi')
+
+
+#a=400
+#b=50
+#c=60
+
+
---
+++ oprators.py
@@ -0,0 +1,165 @@
+#Arithmetic Operator (+ , - , * , / , // , ** , %)
+
+a=4
+b=5
+print(a+b) #Addition(+)
+
+a=6
+b=4
+print(a-b) #Subtraction(-)
+
+a=4
+b=5
+print(a*b) #Multiplication(*)
+
+a=20
+b=5
+print(a/b) #Division(/)
+
+a=23
+b=5
+print(a//b) #Floor Division(//)
+
+a=3
+print(a**3) #Exponentiation(**)
+
+a=24
+b=5
+print(a%b) #Exponentiation(%)
+
+
+#Relational Operator (> , < , >= , <= , == , !=)
+
+a=5
+b=4
+print(a>b) #Greater Than(>)
+
+a=5
+b=4
+print(a<b) #Less Than(<)
+
+a=5
+b=4
+print(a>=b) #Greater Than or Equal To(>=)
+
+a=4
+b=4
+print(a<=b) #Less Than or Equal To(<=)
+
+a=4
+print(a==5) #Equal To(==)
+
+a=5
+print(a!=6) #Not Equal To(!=)
+
+
+# Logical Opeartor (and , or , not)
+
+a=5
+b=6
+c=7
+print(a<b and b>c) #AND Operation(and)
+
+a=5
+b=6
+c=7
+print(a<b or b>c) #OR Operation(or)
+
+a=5
+b=6
+print(not a<b) #NOT Operation(not)
+
+
+#Bitwise Operator (& , | , ~ , ^ , >> , <<)
+
+a=10
+b=4
+print(a&b) #Bitwise AND(&)
+
+a=10
+b=4
+print(a|b) #Bitwise OR(|)
+
+a=10
+print(~a) #Bitwise NOT(~)
+
+a=10
+b=4
+print(a^b) #Bitwise XOR(^) 
+
+a=10
+print(a>>2) #Bitwise Right Shift(>>)
+
+a=5
+print(a<<2) #Bitwise Left Shift(<<)
+
+
+#Assignment Operator (= , += , -= , *= , /= , %= , //= , **= , &= , ^= , >>= , <<=)
+
+a=4
+print(a) #Simple Assignment Operator(=)
+
+a=4
+a += 3
+print(a) #Add and Equal Operator(+=)
+
+a=4
+a -= 3
+print(a) #Subtract and Equal Operator(-=)
+
+a=4
+a *= 3
+print(a) #Asterisk and Equal Operator(*=)
+
+a=4
+a /= 3
+print(a) #Divide and Equal Operator(/=)
+
+a=4
+a %= 3
+print(a) #Modulus and Equal Operator(%=)
+
+a=4
+a //= 3
+print(a) #Double Divide and Equal Operator(//=)
+
+a=4
+a **= 3
+print(a) #Exponent Assign Operator(**=)
+
+a=4
+a &= 3
+print(a) #Bitwise AND Operator(&=)
+
+a=4
+a |= 3
+print(a) #Bitwise OR Operator(|=)
+
+a=4
+a ^= 3
+print(a) #Bitwise XOR Assignment Operator(^=)
+
+a=4
+a >>= 3
+print(a) #Bitwise Right Shift Assignment Operator(>>=)
+
+a=4
+a <<= 3
+print(a) #Bitwise Left Shift Assignment Operator(<<=)
+
+
+#Membership Operator (in , not in)
+
+a='Pythonclass'
+print('t' in a) #In Operator(in)
+
+a='Pythonclass'
+print('c' not in a) #Not In Operator(not in)
+
+
+#Identity Operator (is , is not)
+
+
+
+
+
